01f1eb3ce7831c40316d75868f946c6fa2948543
deepak <deepak20044002@gmail.com>
2025-06-14 17:17:30 +0530

    web scrapper

---
+++ books/pipelines.py
@@ -0,0 +1,50 @@
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+
+
+# useful for handling different item types with a single interface
+from itemadapter import ItemAdapter
+import pymongo
+import hashlib
+from scrapy.exceptions import  DropItem
+
+
+
+class BooksPipeline:
+    COLLECTION_NAME = "books"
+    def __init__(self,mongo_url, mongo_db):
+        self.mongo_url = mongo_url
+        self.mongo_db = mongo_db
+
+    @classmethod
+    def from_crawler(cls,crawler):
+        return cls(
+            mongo_url=crawler.settings.get("MONOGO_URL"),
+            mongo_db= crawler.settings.get("MONGO_DATABASE"),
+        )
+
+    def open_spider(self, spider):
+        self.client = pymongo.MongoClient(self.mongo_url)
+        self.db = self.client[self.mongo_db]
+
+
+    def close_spider(self, spider):
+        self.client.close()
+
+
+    def process_item(self, item, spider):
+        item_id = self.compute_item_id(item)
+        item_dict = ItemAdapter(item).asdict()
+
+        self.db[self.COLLECTION_NAME].update_one(
+            filter={"_id": item_id},
+            update={"$set": item_dict},
+            upsert=True
+        )
+        return item
+
+    def compute_item_id(self, item):
+        url = item["url"]
+        return hashlib.sha256(url.encode("utf-8")).hexdigest()
